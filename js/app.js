0cce084936b5deb12fca62258ac1e2acb4add93f
jamill6654 <jamill6654@gmail.com>
2021-12-20 14:23:41 -0500

    trying to fix multiple event listeners

--- js/app.js
+++ js/app.js
@@ -4,14 +4,15 @@ const startButton = document.querySelector('.btn__reset');
 const overlay = document.querySelector('#overlay');
 let missed = 0;
 const phrases = ['no pain no gain', 'what doesnt kill you makes you stronger', 'one bird in the hand equals two in the bush', 'dont count your eggs before theyre hatched', 'you are what you eat'];
-let showCon = document.querySelectorAll('.show');
 
-
+//MAIN START SCREEN
 startButton.addEventListener('click', (e) => {
     overlay.style.display = 'none';
 })
 
+
 startAgain ();
+//MAIN FUNCTION
 function startAgain () {
 function getRandomPhraseAsArray(arr){
     let randPhrase = phrases[Math.floor(Math.random()*arr.length)];
@@ -19,6 +20,7 @@ function getRandomPhraseAsArray(arr){
     return charArr;
 }
 
+//ADDING RANDOM PHRASE TO SCREEN
 function addPhraseToDisplay(arr){
     let ul = document.querySelector('#phrase');
 
@@ -37,6 +39,7 @@ function addPhraseToDisplay(arr){
 const randphrases = getRandomPhraseAsArray(phrases);
 addPhraseToDisplay(randphrases);
 
+//CHECK IF LETTER MATCHES RETURNS LETTER OR NULL
 function checkLetter(btn){
     let check = document.querySelectorAll('li');
     let match = null;
@@ -49,40 +52,52 @@ function checkLetter(btn){
     }
     return match;
 }
-
+//VAR TO HOLD PHRASE LENGTH
 let letterCon = document.querySelectorAll('.letter');
 
+//REFRESH FUNCTION FOR PLAYING AGAIN
+//CALLS STARTAGAIN TO START MAIN FUNCTION
 function refresh(){
     startButton.addEventListener('click', (e) => {
+        //TRYING TO REMOVE EVENT LISTENER AND FAILING
+        qwerty.removeEventListener('click', e);
         for (let i = 0; i < 26; i++){
             let toChange = document.getElementsByTagName('button')[i]
             toChange.classList.remove('chosen');
+            toChange.disabled = false;
         }
         let ul = document.querySelector('#phrase');
         ul.innerHTML = '';
         missed = 0;
         for (let i = 0; i < 5; i++){
-            let tries = document.getElementsByClassName('tries')[i];
-            tries.style.display = '';
+            let tries = document.getElementsByTagName('img')[i];
+            tries.setAttribute('src', 'images/liveHeart.png');
         }
+
         overlay.style.display = 'none';
         startAgain();
     })
 }
 
+/*KEYBOARD EVENT LISTENER
+*INCREASES MISSED COUNTER
+*CHECKS WIN/LOSE CON AND CALLS REFRESH TO PLAY AGAIN
+*/
 qwerty.addEventListener('click', (e)=> {
-    if(e.target.tagName === 'BUTTON' && e.target.className !== 'chosen'){
+  
+    if(e.target.tagName === 'BUTTON'){
     let checking = checkLetter(e.target);
 
         if(checking === null){
-            let tries = document.getElementsByClassName('tries')[missed];
-            tries.style.display = 'none';
+            let tries = document.getElementsByTagName('img')[missed];
+            tries.setAttribute('src', 'images/lostHeart.png');
             missed++;
         }
-    
+        
         e.target.className = "chosen";
-        let showCon = document.querySelectorAll('.show');
+        e.target.disabled = true;
 
+        let showCon = document.querySelectorAll('.show');
         if(letterCon.length === showCon.length){
             startButton.className += 'win';
             document.querySelector('h2').innerHTML = "YOU HAVE WON";
